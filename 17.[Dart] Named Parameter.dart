4d8f963515e698e80da557847fcef0cc7e2e579e
kaomao1234 <boripats63@nu.ac.th>
2021-04-20 15:51:53 +0700

    add all

--- 17.[Dart] Named Parameter.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  showData(name: 'Pure', age: '19', city: 'Pitsanulork');
-}
-showData({name,city,age}) {
-  print('Name = $name\tCity = $city\tAge = $age');
-}
--- 18.[Dart] First-Class Function.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  var result = getthree; //todo ตัวแปลมีคุณสมบัติเป็นฟังก์ชัน
-  print(result(3));
-}
-
-getthree(num) => num * 3;
