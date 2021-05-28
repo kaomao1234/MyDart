121087ac95ff6ac2447c3ffa58578e830d27daae
kaomao1234 <boripats63@nu.ac.th>
2021-05-29 01:02:32 +0700

    Update 27.[Dart] แปลง Map เป็น List.dart

--- 27.[Dart] แปลง Map เป็น List.dart
+++ 27.[Dart] แปลง Map เป็น List.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  Map<String, String> color = {'#FF0000': 'red', '#FFFF00': 'yellow'};
+  Map<String, String> color = {'#FF0000': 'red', '#FFFF00': 'yellow'}; 
   print(color.values);
   var code_color = color.keys.toList();//todo แปลงเป็นlist
   print(code_color);
