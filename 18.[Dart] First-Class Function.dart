847ffd6ee7741e971b39af8bf458fc41f5a10545
kaomao1234 <boripats63@nu.ac.th>
2021-08-09 02:12:40 +0700

    add all

--- 18.[Dart] First-Class Function.dart
+++ 18.[Dart] First-Class Function.dart
@@ -1,6 +1,7 @@
+import 'dart:io'
 void main() {
-  var result = getthree; //todo ตัวแปลมีคุณสมบัติเป็นฟังก์ชัน
-  print(result(3));
+  // var result = getthree; //todo ตัวแปลมีคุณสมบัติเป็นฟังก์ชัน 
+  // print(result(3));
 }
-
-getthree(num) => num * 3;
+int
+// getthree(num) => num * 3;
